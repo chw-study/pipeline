1d4b31779a9464054073e7e62d921389178057ef
Nandan Rao <nandanmarkrao@gmail.com>
2018-08-17 21:45:44 +0200

    new cron scripts

---
+++ start-cron-callcenter.sh
@@ -0,0 +1,1 @@
+hyper cron create --minute=0 --hour=1 --name hw-callcenter --env-file .env --size M3 nandanrao/hw-pipeline python callcenter.py
--- start-cron.sh
+++
@@ -1,1 +0,0 @@
-hyper cron create --minute=0 --hour=1 --name hw-payments --env-file .env --size M3 nandanrao/hw-payments
